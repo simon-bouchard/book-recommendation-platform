64ea38f789997aa961f295b340ef23b4891f66d9
Simon Bouchard <simon.bouchard31@gmail.com>
2025-10-02 17:11:46 -0400

    prepare __init__

---
+++ app/agents/infrastructure/recsys/__init__.py
@@ -0,0 +1,9 @@
+from .recommendation_agent import RecommendationAgent
+from .retrieval_agent import RetrievalAgent
+from .curation_agent import CurationAgent
+
+__all__ = [
+    "RecommendationAgent",
+    "RetrievalAgent", 
+    "CurationAgent",
+]
