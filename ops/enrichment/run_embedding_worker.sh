b0dd3fe1655283fe52dca295ff7116e31310379b
Simon Bouchard <simon.bouchard31@gmail.com>
2025-11-06 05:40:56 +0100

    switch from test to real

--- ops/enrichment/run_embedding_worker.sh
+++ ops/enrichment/run_embedding_worker.sh
@@ -9,7 +9,6 @@ source /home/simon/miniconda3/bin/activate bookrec-api
 # Run the worker with logging
 python -m app.semantic_index.embedding.worker \
     --mode incremental \
-    --limit 20 \
     --tags-version v2 \
     >> logs/embedding_worker.log 2>&1
 
