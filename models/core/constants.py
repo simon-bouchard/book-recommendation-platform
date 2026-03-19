082196be2c73e73da2232730ae37f12fcf109933
Simon Bouchard <simon.bouchard31@gmail.com>
2026-03-18 21:15:15 -0400

    perf(models): remove torch from constants file to remove torch dependency in main app

--- models/core/constants.py
+++ models/core/constants.py
@@ -4,8 +4,5 @@ System-wide constants used throughout the models package.
 """
 
 import os
-import torch
 
 PAD_IDX: int = int(os.getenv("PAD_IDX", "0"))
-
-DEVICE: str = "cuda" if torch.cuda.is_available() else "cpu"
